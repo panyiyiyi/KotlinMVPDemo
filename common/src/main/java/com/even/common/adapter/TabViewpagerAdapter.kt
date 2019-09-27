be97db3f30d798f5441712614bf5fa2aece8ba8a
even <1171867212@qq.com>
2019-09-27 16:12:11 +0800

    update

--- common/src/main/java/com/even/common/adapter/TabViewpagerAdapter.kt
+++ common/src/main/java/com/even/common/adapter/TabViewpagerAdapter.kt
@@ -3,18 +3,18 @@ package com.even.common.adapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import com.even.common.base.BaseFragment
 
 /**
  * @author  Created by Even on 2019/8/14
  *  Email: emailtopan@163.com
  *  Fragment和ViewPager适配器
  */
-class TabViewpagerAdapter(
+class TabViewpagerAdapter<T : Fragment>(
     fm: FragmentManager,
-    var fgLists: List<BaseFragment>,
-    var titleNames: Array<String>
-) : FragmentPagerAdapter(fm) {
+    var fgLists: List<T>,
+    var titleNames: List<String>,
+    private var behavior: Int?
+) : FragmentPagerAdapter(fm, behavior ?: BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
     override fun getItem(position: Int): Fragment {
         return fgLists[position]
     }
